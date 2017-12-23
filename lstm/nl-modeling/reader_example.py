1e86cf917b1f7f6654d7555bd07461fcf7b0716c
widiot <984209543@qq.com>
2017-12-23 17:51:40 +0800

    实现了PTB数据集的读取代码示范

---
+++ lstm/nl-modeling/reader_example.py
@@ -0,0 +1,28 @@
+import reader
+import tensorflow as tf
+
+# 数据路径
+DATA_PATH = 'simple-examples/data/'
+
+# 读取原始数据
+train_data, valid_data, test_data, _ = reader.ptb_raw_data(DATA_PATH)
+
+with tf.Session() as sess:
+    tf.global_variables_initializer().run()
+
+    # 将数据组织成batch大小为4，截断长度为5的数据组，要放在开启多线程之前
+    batch = reader.ptb_producer(train_data, 4, 5)
+
+    # 开启多线程
+    coord = tf.train.Coordinator()
+    threads = tf.train.start_queue_runners(coord=coord)
+
+    # 读取前两个batch，其中包括每个时刻的输入和对应的答案，ptb_producer()会自动迭代
+    for i in range(2):
+        x, y = sess.run(batch)
+        print('x:', x)
+        print('y:', y)
+
+    # 关闭多线程
+    coord.request_stop()
+    coord.join(threads)
